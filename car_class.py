d5857c7da291ba1e80b5b0033f4124769758a461
aparish0328 <75654412+aparish0328@users.noreply.github.com>
2023-03-24 18:25:54 -0600

    Add files via upload
    This is the file that defines the class "car" that is used in the car_db.

---
+++ car_class.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, make, model, hrspr, weight, accel):
+        self.make=make
+        self.model=model
+        self.hrspr=hrspr
+        self.weight=weight
+        self.accel=accel
